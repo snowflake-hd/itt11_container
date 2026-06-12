ccef64a0fd48f1770e05bdede114838315369b08
snowflake-hd <david.patrick.gonska@gmail.com>
2026-06-12 23:47:49 +0200

    fix: adding mongodb cmd to default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -7,6 +7,7 @@ pkgs.mkShell {
   echo -e "=== Available Commands ==="
   echo "  nix develop - this shell"
   echo "  nix develop .#mariadb - starts mariadb development environment with some tools"
+  echo "  nix develop .#mongodb - starts mongodb development environment with some tools"
   echo ""
   exit 0
  '';
